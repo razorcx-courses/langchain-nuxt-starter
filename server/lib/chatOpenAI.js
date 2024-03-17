398233a50e00c97bf863a08726ba09109f1fcffe
razorcx-courses <100399176+razorcx-courses@users.noreply.github.com>
2024-03-17 09:15:45 -0700

    move openai model to server

--- server/lib/chatOpenAI.js
+++ server/lib/chatOpenAI.js
@@ -2,7 +2,7 @@ import { ChatOpenAI } from "@langchain/openai";
 
 //https://platform.openai.com/docs/models/gpt-3-5-turbo
 export const chatOpenAIModel = new ChatOpenAI({
-  openAIApiKey: import.meta.env.VITE_APP_OPEN_API_KEY,
+  openAIApiKey: process.env.NUXT_OPEN_API_KEY,
   modelName: "gpt-3.5-turbo",
   temperature: 0,
 });
