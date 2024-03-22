558a08757ebf6553ce6b5e2fc038f02cdd6d10c3
razorcx-courses <100399176+razorcx-courses@users.noreply.github.com>
2024-03-21 19:48:25 -0700

    fix endpoint queries

--- composables/useApiEndpoints.js
+++ composables/useApiEndpoints.js
@@ -1,15 +1,16 @@
+//https://stackoverflow.com/questions/77022535/how-to-fix-cors-error-on-3rd-party-api-call-in-nuxt-3
 export const useApiEndpoints = () => {
   return {
-    joke: (thing) => "/api/joke/" + thing,
+    joke: (thing) => "api/joke/" + thing,
     prompts: {
       simple: (product) => "/api/prompts/simple/" + product,
       noInput: (template) => "/api/prompts/noinput?template=" + template,
       oneInput: (adjective) =>
         "/api/prompts/oneinput?template=" +
-        `Tell+me+a+{adjective}+joke.&${adjective}`,
+        `Tell+me+a+{adjective}+joke.&adjective=${adjective}`,
       multiInput: (adjective, noun) =>
         "/api/prompts/multiinput?template=" +
-        `Tell me a {adjective} joke about a {noun}.&${adjective}&${noun}`,
+        `Tell me a {adjective} joke about a {noun}.&adjective=${adjective}&noun=${noun}`,
     },
   };
 };
