ab3cc1d3ec2c589dd5357a597a50c174d255514f
razorcx-courses <100399176+razorcx-courses@users.noreply.github.com>
2024-03-22 16:21:12 -0700

    implement execute and useAsyncData with watch,  transform & cached data options

--- components/CompanyName.vue
+++ components/CompanyName.vue
@@ -9,7 +9,8 @@
       <template v-if="response">
         <div>
           <p class="font-bold text-lg">useFetch Example</p>
-          <p class="text-bold text-pink-500 ml-2">{{ response }}</p>
+          <p class="text-bold text-pink-500 ml-2">{{ response.input }}</p>
+          <p class="text-xs ml-2">Last fetched: {{ response.fetchedAt }}</p>
         </div>
       </template>
     </div>
@@ -19,13 +20,35 @@
 <script setup>
 const chatWindowTitle = ref("Simple Prompt Template Example");
 const chatWindowDesciption = ref("Company Name from {productType}");
-const response = ref();
-const modelValue = ref("boxes");
+const modelValue = ref("toys");
+
+const { data: response, execute } = await useAsyncData(
+  "company",
+  () => $fetch("/api/prompts/simple/" + modelValue.value),
+  {
+    watch: [modelValue],
+    transform(input) {
+      console.log(input);
+      return {
+        input,
+        fetchedAt: new Date(),
+      };
+    },
+    getCachedData(key, nuxtApp) {
+      console.log(key, nuxtApp.payload.data[key], nuxtApp.static.data[key]);
+      const data = nuxtApp.payload.data[key] || nuxtApp.static.data[key];
+      if (!data) {
+        return;
+      }
+
+      //just return for now and do refetch
+      return;
+    },
+  }
+);
 
 const onGetResponse = async () => {
-  //use $fetch because dont need a reactive response
-  response.value = await $fetch("/api/prompts/simple/" + modelValue.value);
-
+  await execute();
   console.log(response.value);
 };
 </script>
