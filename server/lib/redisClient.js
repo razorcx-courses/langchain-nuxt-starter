da0c60c665bb7c2e6ecf347ee07b899b1e98ec66
razorcx-courses <100399176+razorcx-courses@users.noreply.github.com>
2024-03-21 19:48:03 -0700

    add runtime config

--- server/lib/redisClient.js
+++ server/lib/redisClient.js
@@ -1,23 +1,18 @@
 import { Redis } from "@upstash/redis";
 
+const config = useRuntimeConfig();
+
+
 //https://vitejs.dev/guide/env-and-mode
 export const redisClient = () => {
-  
-  if (
-    ! process.env.NUXT_UPSTASH_REDIS_REST_URL ||
-    ! process.env.NUXT_UPSTASH_REDIS_REST_TOKEN
-  ) {
+  if (!config.upstashRedisRestUrl || !config.upstashRedisRestToken) {
     throw new Error(
       "UPSTASH_REDIS_REST_URL and UPSTASH_REDIS_REST_TOKEN must be set in the environment"
     );
   }
   const client = new Redis({
-    url: process.env.NUXT_UPSTASH_REDIS_REST_URL,
-    token: process.env.NUXT_UPSTASH_REDIS_REST_TOKEN,
+    url: config.upstashRedisRestUrl,
+    token: config.upstashRedisRestToken,
   });
   return client;
 };
-
-
-
-
