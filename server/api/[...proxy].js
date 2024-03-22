e5de0566565b2d72bfb3e7cb5b51c89eaceeef26
razorcx-courses <100399176+razorcx-courses@users.noreply.github.com>
2024-03-22 11:37:32 -0700

    fix proxy targetUrl bug

--- server/api/[...proxy].js
+++ server/api/[...proxy].js
@@ -2,14 +2,15 @@ export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig();
 
   const path = getRouterParam(event, "proxy");
+  const url = getRequestURL(event);
 
   // Concatenate the apiBase with the modified path, ensuring there's a "/" between them
-  const target = `${config.public.apiProxyBase}/${path}`;
+  const target = `${config.public.apiProxyBase}/${path}${url.search}`;
 
   // Ensure there are no double slashes (other than the http:// part)
   const targetUrl = new URL(target.replace(/([^:]\/)\/+/g, "$1"));
 
-  console.log({ path, targetUrl });
+  console.log({ url, params: event.context.params, path, target });
 
   return proxyRequest(event, targetUrl);
 });
