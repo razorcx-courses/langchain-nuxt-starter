751030658b5543bfa46cfd637e2aa70cdc3e9b3a
razorcx-courses <100399176+razorcx-courses@users.noreply.github.com>
2024-03-24 06:01:05 -0700

    add lanchainLinks to appdata

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,13 @@
   <section id="chat" class="pt-2 md:pt-4 px-4 pb-12 max-w-sm mx-auto">
     <div id="chat-card" class="flex flex-col rounded-md overflow-hidden gap-6">
       <ChatBoxTitle></ChatBoxTitle>
-      <div class="flex flex-col gap-2 text-center bg-white/10 p-6 rounded-lg">
+      <div class="flex flex-col gap-2 bg-white/10 p-6 rounded-lg grow">
         <template v-for="link in links" :key="link.name">
-          <NuxtLink class="text-white cursor-pointer hover:text-pink-400" :to="link.to"
-            ><p>{{ link.name }}</p>
-          </NuxtLink>
+          <div class="flex items-center justify-center cursor-pointer">
+            <NuxtLink class="text-white hover:text-pink-400" :to="link.to"
+              >{{ link.name }}
+            </NuxtLink>
+          </div>
         </template>
       </div>
     </div>
@@ -14,24 +16,13 @@
 </template>
 
 <script setup>
+import { exampleLinks } from "../appdata/lanchainLinks"
 const title = useTitle();
 const description = useDescription();
 
 title.value = "Langchain | Nuxt | TailwindCSS";
 description.value = "Chatbot Examples";
 
-const links = ref();
+const links = ref(exampleLinks);
 
-links.value = [
-  { name: "Joke", to: "/langchain/joke" },
-  { name: "Chat Prompts", to: "/langchain/chatprompts" },
-  { name: "CSV List", to: "/langchain/csvlist" },
-  { name: "Few Shot", to: "/langchain/fewshot" },
-  { name: "JSON Parser", to: "/langchain/jsonparser" },
-  { name: "Pipeline", to: "/langchain/pipeline" },
-  { name: "Prompting Functions", to: "/langchain/promptingfunctions" },
-  { name: "RAG", to: "/langchain/rag" },
-  { name: "Prompts", to: "/langchain/prompts" },
-  { name: "Simple Prompts", to: "/langchain/prompts/simple" },
-];
 </script>
