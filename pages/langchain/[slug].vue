54e013cad1195a6150a4aee94a91ecd731806e6e
razorcx-courses <100399176+razorcx-courses@users.noreply.github.com>
2024-06-10 11:53:07 -0700

    remove unused components

--- pages/langchain/[slug].vue
+++ pages/langchain/[slug].vue
@@ -1,16 +1,6 @@
 <template>
   <div>
     <component v-if="showPage(page)" :is="page"></component>
-    <!-- <Joke v-if="showPage('joke')" page="joke"></Joke> -->
-    <!-- <ChatPrompts v-else-if="showPage('chatprompts')" page="chatprompts"></ChatPrompts>
-    <CSVListOutputParser v-else-if="showPage('csvlist')" page="csvlist"></CSVListOutputParser>
-    <FewShotPrompts v-else-if="showPage('fewshot')" page="fewshot"></FewShotPrompts>
-    <JsonOutputParser v-else-if="showPage('jsonparser')" page="jsonparser"></JsonOutputParser>
-    <PipelinePrompts v-else-if="showPage('pipeline')" page="pipeline"></PipelinePrompts>
-    <PromptingFunctions
-      v-else-if="showPage('promptingfunctions')" page="promptingfunctions"
-    ></PromptingFunctions>
-    <RAGwithPDF v-else-if="showPage('rag')" page="rag"></RAGwithPDF> -->
   </div>
 </template>
 
