0a2a822584cc20e5b9e20e8078fb365d092a307b
razorcx-courses <100399176+razorcx-courses@users.noreply.github.com>
2024-06-10 11:59:30 -0700

    replace get with post request

--- server/api/[...proxy].post.ts
+++ server/api/[...proxy].post.ts
@@ -1,13 +1,22 @@
 import { getProxyUrl } from "../lib/proxyUrl";
 
 export default defineEventHandler(async (event) => {
-  const { targetUrl } = getProxyUrl(event);
+  const { docs, prompt } = await readBody(event);
+  if(!docs) return;
 
+  const { targetUrl } = getProxyUrl(event);
   const { apiKey } = useRuntimeConfig(event);
 
   //edited Sep 13, 2023
   //https://stackoverflow.com/questions/77068584/how-to-change-request-headers-with-server-middleware
   Object.assign(event.node.req.headers, { Authorization: apiKey });
 
-  return proxyRequest(event, targetUrl.toString());
+  //const NUXT_API_PROXY_BASE = "http://localhost:8080/api/rag/pdf";
+
+  const res = await $fetch(targetUrl.toString(), {
+    method: "POST",
+    body: { docs: docs, prompt: prompt },
+  });
+
+  return res;
 });
