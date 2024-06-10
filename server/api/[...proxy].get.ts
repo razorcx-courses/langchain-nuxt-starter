23324782e5becb6554bc4610104617d4e7a78a3f
razorcx-courses <100399176+razorcx-courses@users.noreply.github.com>
2024-06-10 16:03:08 -0700

    fix for .env variables

--- server/api/[...proxy].get.ts
+++ server/api/[...proxy].get.ts
@@ -1,8 +1,6 @@
 import { getProxyUrl } from "../lib/proxyUrl";
 
 export default defineEventHandler(async (event) => {
-  console.log(getRouterParam(event, "proxy"));
-
   const { targetUrl } = getProxyUrl(event);
 
   const { apiKey } = useRuntimeConfig(event);
@@ -11,10 +9,6 @@ export default defineEventHandler(async (event) => {
   //https://stackoverflow.com/questions/77068584/how-to-change-request-headers-with-server-middleware
   Object.assign(event.node.req.headers, { Authorization: apiKey });
 
-  console.log(apiKey, targetUrl.toString());
-
   const res = await proxyRequest(event, targetUrl.toString());
-
-  //const res = await $fetch(targetUrl.toString())
   return res;
 });
